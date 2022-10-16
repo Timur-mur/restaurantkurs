bf73555393e25769b90efb6ea93ee7fa724d5c6a
Timur-mur <fattahowtimur@yandex.ru>
2022-10-16 23:10:51 +0300

    second commit

---
+++ restaurant_vue/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="wrapper">
+    <router-view/>
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"><strong> Эmurr </strong></router-link>
+
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div class="navbar-menu" id="navbar-menu">
+        <div class="navbar-end">
+          <router-link to="/menu"></router-link>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<style lang="scss">
+@import "../node_modules/bulma";
+</style>
